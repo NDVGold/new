2ada85c439bf2588dbee0a34c06a8da86132ed8d
Dan <fashion_01@bk.ru>
2023-09-14 14:14:12 +0600

    первое приложение

--- js/new.js
+++ js/new.js
@@ -1,3 +1,16 @@
 'user strict';
 
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?', '');
 
+let personalMovieDB = {
+	'count': numberOfFilms,
+	'movies': {},
+	'actors': {},
+	'genres': [],
+	'privat': false
+};
+
+// let lastFilm = prompt('Название последнего просмотренного фильма?'), appreFilm = prompt('как вы его оцените?');
+
+personalMovieDB.movies[prompt('Название последнего просмотренного фильма?', '')] = prompt('как вы его оцените?', '');
+console.log(personalMovieDB);
